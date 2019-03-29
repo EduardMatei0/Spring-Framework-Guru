9fca2c94c374962e74c9e01a0e8b33854e49641a
EduardMatei <eduardmatei0@yahoo.com>
2019-03-29 11:58:57 +0200

    Adding Repositories

---
+++ src/main/java/com/eduardmatei/spring5webapp/repositories/AuthorRepository.java
@@ -0,0 +1,7 @@
+package com.eduardmatei.spring5webapp.repositories;
+
+import com.eduardmatei.spring5webapp.model.Author;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AuthorRepository extends CrudRepository<Author, Long> {
+}
